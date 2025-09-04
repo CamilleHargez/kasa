a4e11d90491443da9e6ebb127db05d2783888b5f
Camille Hargez <camille.hargez@gmail.com>
2025-09-04 15:42:49 +0200

    correction tests files

--- Kasa/src/components/partials/__tests__/collapse.test.tsx
+++ Kasa/src/components/partials/__tests__/collapse.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen, fireEvent } from "@testing-library/react"
 import { describe, it, expect, vi } from "vitest"
 import Collapse from "../Collapse"
+import '@testing-library/jest-dom'
 
 
 vi.mock("@/assets/arrow_top.png", () => ({
